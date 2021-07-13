fbe6c8198d34c93ac74f1ed973321a8bd32049e2
Prantik <shahnoorprantik@gmail.com>
2021-07-13 09:42:58 +0600

    Encapsulation

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -4,6 +4,8 @@ package com.prantik;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Bismillah");
+        Person person1 = new Person();
+        person1.setName("Prantik");
+        System.out.println(person1.getName());
     }
 }
