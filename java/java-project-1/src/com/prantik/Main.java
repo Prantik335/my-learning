a6d9400f24d079b554dcbf8f3ee94f7d87cf0c97
Prantik <shahnoorprantik@gmail.com>
2021-07-14 08:49:07 +0600

    Java/java-project-1 - Interface

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -1,25 +1,53 @@
 package com.prantik;
 
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Pig pig = new Pig();
         pig.animalSound();
         pig.sleep();
+
+        DemoClass obj = new DemoClass();
+        obj.myMethod();
+        obj.myOtherMethod();
     }
 }
 
-abstract class Animal {
-    public abstract void animalSound();
+interface Animal {
+    public void animalSound();
+
+    public void sleep();
+}
+
+class Pig implements Animal {
+
+    @Override
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+
+    @Override
     public void sleep() {
         System.out.println("Zzz");
     }
 }
 
-class Pig extends Animal {
-    public void animalSound() {
-        System.out.println("The ping says: we wee");
-    }
+interface If1 {
+    public void myMethod();
 }
 
+interface If2 {
+    public void myOtherMethod();
+}
+
+class DemoClass implements If1, If2 {
+
+    @Override
+    public void myMethod() {
+        System.out.println("Some text..");
+    }
+
+    @Override
+    public void myOtherMethod() {
+        System.out.println("Some other text...");
+    }
+}
