8d8f007b95650964f62f45372e92fe1325aef4a0
Prantik <shahnoorprantik@gmail.com>
2021-07-14 08:58:34 +0600

    Java/java-projec-1 - Enums

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -1,53 +1,16 @@
 package com.prantik;
 
 public class Main {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
     public static void main(String[] args) {
-        Pig pig = new Pig();
-        pig.animalSound();
-        pig.sleep();
-
-        DemoClass obj = new DemoClass();
-        obj.myMethod();
-        obj.myOtherMethod();
+        for(Level level: Level.values()) {
+            System.out.println(level);
+        }
     }
 }
 
-interface Animal {
-    public void animalSound();
-
-    public void sleep();
-}
-
-class Pig implements Animal {
-
-    @Override
-    public void animalSound() {
-        System.out.println("The pig says: wee wee");
-    }
-
-    @Override
-    public void sleep() {
-        System.out.println("Zzz");
-    }
-}
-
-interface If1 {
-    public void myMethod();
-}
-
-interface If2 {
-    public void myOtherMethod();
-}
-
-class DemoClass implements If1, If2 {
-
-    @Override
-    public void myMethod() {
-        System.out.println("Some text..");
-    }
-
-    @Override
-    public void myOtherMethod() {
-        System.out.println("Some other text...");
-    }
-}
