fbcebee82bfda26cbcb638168878f7df4ae5af14
Prantik <shahnoorprantik@gmail.com>
2021-07-14 09:15:18 +0600

    Java/java-projec-1 - Scanner

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -1,16 +1,22 @@
 package com.prantik;
 
+import java.util.Scanner;
+
 public class Main {
-    enum Level {
-        LOW,
-        MEDIUM,
-        HIGH
-    }
 
     public static void main(String[] args) {
-        for(Level level: Level.values()) {
-            System.out.println(level);
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter your Salary: ");
+        double salary = scanner.nextDouble();
+
+        System.out.println("\nDetails:\nUsername: " + username + "\nAge: " + age + "\nSalary: " + salary);
     }
 }
 
