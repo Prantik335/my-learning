ae0e2e470d1a3b9b021003a6add428739f89d033
Prantik <shahnoorprantik@gmail.com>
2021-07-15 08:28:11 +0600

    java/java-project-1 - Wrapper Class

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -7,21 +7,12 @@ import java.util.Iterator;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(12);
-        numbers.add(8);
-        numbers.add(2);
-        numbers.add(23);
+        Integer myInt = 5000;
+        Double myDouble = 5.99;
+        Character myChar = 'P';
 
-        Iterator<Integer> it = numbers.iterator();
-
-        while(it.hasNext()) {
-            Integer i = it.next();
-            if(i < 10) {
-                it.remove();
-            }
-        }
-
-        System.out.println(numbers);
+        System.out.println(myInt.toString().length());
+        System.out.println(myDouble);
+        System.out.println(myChar);
     }
 }
