254b71689d72087bd9010acd07e965d145831524
Prantik <shahnoorprantik@gmail.com>
2021-07-15 08:37:30 +0600

    java/java-project-1 - Exceptions

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -1,18 +1,26 @@
 package com.prantik;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
-        Integer myInt = 5000;
-        Double myDouble = 5.99;
-        Character myChar = 'P';
+        try {
+            int[] numbers = {1, 2, 3};
+            System.out.println(numbers[10]);
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("The 'try catch' is finished");
+        }
 
-        System.out.println(myInt.toString().length());
-        System.out.println(myDouble);
-        System.out.println(myChar);
+        checkAge(16);
+    }
+
+    public static void checkAge(int age) {
+        if(age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
     }
 }
