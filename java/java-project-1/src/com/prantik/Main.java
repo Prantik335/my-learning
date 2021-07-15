a08a4f2466dc39efe547145eba441c6f263baa71
Prantik <shahnoorprantik@gmail.com>
2021-07-15 08:19:02 +0600

    java/java-project-1 - Iterator

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -1,17 +1,27 @@
 package com.prantik;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
-        HashSet<String> friends = new HashSet<String>();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(12);
+        numbers.add(8);
+        numbers.add(2);
+        numbers.add(23);
 
-        friends.add("Nahin");
-        friends.add("Sciene");
-        friends.add("Asif");
-        friends.add("Nahid");
+        Iterator<Integer> it = numbers.iterator();
 
-        System.out.println(friends.contains("Prantik"));
+        while(it.hasNext()) {
+            Integer i = it.next();
+            if(i < 10) {
+                it.remove();
+            }
+        }
+
+        System.out.println(numbers);
     }
 }
