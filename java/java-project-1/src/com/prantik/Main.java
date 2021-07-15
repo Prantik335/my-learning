3026f69a2e4e851e4794ad37eb02a81cff8832a6
Prantik <shahnoorprantik@gmail.com>
2021-07-15 08:06:25 +0600

    java/java-project-1 - HashSet

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -1,30 +1,17 @@
 package com.prantik;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
-        HashMap<String, String> capitalCities = new HashMap<>();
+        HashSet<String> friends = new HashSet<String>();
 
-        capitalCities.put("Bangladesh", "Dhaka");
-        capitalCities.put("Germany", "Berlin");
-        capitalCities.put("Norway", "Oslo");
-        capitalCities.put("USA", "Washington DC");
+        friends.add("Nahin");
+        friends.add("Sciene");
+        friends.add("Asif");
+        friends.add("Nahid");
 
-        for(String key: capitalCities.keySet()) {
-            System.out.println("key: " + key + " - value: " + capitalCities.get(key));
-        }
-        System.out.println();
-
-        HashMap<String, Integer> students = new HashMap<>();
-
-        students.put("Asif", 18);
-        students.put("Prantik", 16);
-        students.put("Nahin", 17);
-
-        for(String key: students.keySet()) {
-            System.out.println(key + " is " + students.get(key) + " years old.");
-        }
+        System.out.println(friends.contains("Prantik"));
     }
 }
