f2c6fbe83c017fafc72250ef0a5a811480c62604
Prantik <shahnoorprantik@gmail.com>
2021-07-15 10:21:31 +0600

    java/java-project-1 - How to add to numbers

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -1,11 +1,21 @@
 package com.prantik;
 
-import java.io.File;
 
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int x, y, sum;
+
+        System.out.print("Enter a number: ");
+        x = scanner.nextInt();
+        System.out.print("Enter another number: ");
+        y = scanner.nextInt();
+
+        sum = x + y;
+
+        System.out.println("Sum is: " + sum);
     }
 }
