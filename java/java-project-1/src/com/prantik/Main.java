be9be76d30d31d58812e6569d87fdb126f4cc139
Prantik <shahnoorprantik@gmail.com>
2021-07-15 08:00:43 +0600

    java/java-project-1 - HashMap

--- java/java-project-1/src/com/prantik/Main.java
+++ java/java-project-1/src/com/prantik/Main.java
@@ -1,16 +1,30 @@
 package com.prantik;
 
-import java.util.LinkedList;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList<String> cars = new LinkedList<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        System.out.println(cars);
+        HashMap<String, String> capitalCities = new HashMap<>();
+
+        capitalCities.put("Bangladesh", "Dhaka");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+
+        for(String key: capitalCities.keySet()) {
+            System.out.println("key: " + key + " - value: " + capitalCities.get(key));
+        }
+        System.out.println();
+
+        HashMap<String, Integer> students = new HashMap<>();
+
+        students.put("Asif", 18);
+        students.put("Prantik", 16);
+        students.put("Nahin", 17);
+
+        for(String key: students.keySet()) {
+            System.out.println(key + " is " + students.get(key) + " years old.");
+        }
     }
 }
-
