fcb580dfa69f41f96eeadbef9165fbfd5c3b7a31
Prantik <shahnoorprantik@gmail.com>
2021-07-18 15:38:40 +0600

    Section 3: -Layouts

--- android-studio/first-project/app/src/main/java/me/prantik/firstproject/MainActivity.java
+++ android-studio/first-project/app/src/main/java/me/prantik/firstproject/MainActivity.java
@@ -7,13 +7,10 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button2);
     }
 }
