474b3fda696d254e4dad428624d14ede92c28dd8
Prantik <shahnoorprantik@gmail.com>
2021-07-19 08:56:14 +0600

    checkbox

--- android-studio/first-project/app/src/main/java/me/prantik/firstproject/MainActivity.java
+++ android-studio/first-project/app/src/main/java/me/prantik/firstproject/MainActivity.java
@@ -6,30 +6,51 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText name;
     Button ok;
     TextView result;
-    String userName;
+    ImageView image;
+    CheckBox male;
+    CheckBox female;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = findViewById(R.id.editTextName);
         ok = findViewById(R.id.buttonOk);
         result = findViewById(R.id.textViewResult);
+        image = findViewById(R.id.imageView);
+        male = findViewById(R.id.checkBoxMale);
+        female = findViewById(R.id.checkBoxFemale);
+
+        male.setOnClickListener(view -> {
+            if (male.isChecked()) {
+                result.setText("Male");
+                female.setChecked(false);
+            } else {
+                result.setText("What is your gender?");
+            }
+        });
+        female.setOnClickListener(view -> {
+            if (female.isChecked()) {
+                result.setText("Female");
+                male.setChecked(false);
+            } else {
+                result.setText("What is your gender?");
+            }
+        });
 
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                userName = name.getText().toString();
-                result.setText(userName);
+                image.setImageResource(R.drawable.mountain);
             }
         });
     }
