66192b031c15bf579a6f8d4301fd167d7539e8d3
Prantik <shahnoorprantik@gmail.com>
2021-07-19 10:02:15 +0600

    Section 4 completed

--- android-studio/first-project/app/src/main/java/me/prantik/firstproject/MainActivity.java
+++ android-studio/first-project/app/src/main/java/me/prantik/firstproject/MainActivity.java
@@ -1,8 +1,10 @@
 package me.prantik.firstproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -42,14 +44,28 @@ public class MainActivity extends AppCompatActivity {
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(layout, "This is a snackbar message", Snackbar.LENGTH_INDEFINITE)
-                        .setAction("Close", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-
-                            }
-                        }).show();
+                showDialogMessage();
             }
         });
+    }
+
+    private void showDialogMessage() {
+        AlertDialog.Builder alertDialogue = new AlertDialog.Builder(this);
+        alertDialogue.setTitle("Delete")
+                .setMessage("Do you want to delete text?")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        result.setText("");
+                    }
+                }).show();
+
+        alertDialogue.create();
     }
 }
