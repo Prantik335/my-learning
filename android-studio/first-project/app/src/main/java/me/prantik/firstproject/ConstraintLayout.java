25f06c4ecb2f7f137972e1fde508a25699641bac
Prantik <shahnoorprantik@gmail.com>
2021-07-20 08:10:22 +0600

    android/first-project closed

--- android-studio/first-project/app/src/main/java/me/prantik/firstproject/ConstraintLayout.java
+++ android-studio/first-project/app/src/main/java/me/prantik/firstproject/ConstraintLayout.java
@@ -3,12 +3,37 @@ package me.prantik.firstproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 
 public class ConstraintLayout extends AppCompatActivity {
+
+    ListView listView;
+    String countries[];
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout);
+
+        listView = findViewById(R.id.listView);
+        countries = getResources().getStringArray(R.array.countries);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String country = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), "You selected " + country, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
