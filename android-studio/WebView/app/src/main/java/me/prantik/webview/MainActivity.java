323a8ebf7f539bd0bda013b5a61cb7d54c899a4a
Prantik <shahnoorprantik@gmail.com>
2021-07-22 07:34:57 +0600

    Simple browser app by webview closed

--- android-studio/WebView/app/src/main/java/me/prantik/webview/MainActivity.java
+++ android-studio/WebView/app/src/main/java/me/prantik/webview/MainActivity.java
@@ -2,13 +2,56 @@ package me.prantik.webview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    EditText urlEditText;
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        webView = findViewById(R.id.web_view);
+        urlEditText = findViewById(R.id.editTextUrl);
+        searchButton = findViewById(R.id.search_button);
+
+        searchButton.setOnClickListener(view -> {
+            hideKeyboard();
+            search(urlEditText.getText().toString());
+        });
+
+    }
+
+    private void search(String url) {
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    private void hideKeyboard() {
+        View view = getCurrentFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (view != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
