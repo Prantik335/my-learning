bb1c668ab5e90542f218755a997563c09117cb26
Prantik <shahnoorprantik@gmail.com>
2021-07-24 09:44:29 +0600

    android-studio\DifferentScreenSizes created

--- android-studio/DifferentPixelDensities/app/src/main/java/me/prantik/differentpixeldensities/MainActivity.java
+++ android-studio/DifferentPixelDensities/app/src/main/java/me/prantik/differentpixeldensities/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.image_mine);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
     }
 }
