9f48c874c8f513e2b7b1c34a40b55219539f4c24
Prantik <shahnoorprantik@gmail.com>
2021-07-24 08:46:15 +0600

    android-studio\DifferentPixelDensities created

--- android-studio/Internationalization/app/src/main/java/me/prantik/internationalization/MainActivity.java
+++ android-studio/Internationalization/app/src/main/java/me/prantik/internationalization/MainActivity.java
@@ -2,13 +2,69 @@ package me.prantik.internationalization;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    Button show;
+    Switch switchBangla;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("lang", MODE_PRIVATE);
+
+        show = findViewById(R.id.button);
+        switchBangla = findViewById(R.id.switchBangla);
+
+        show.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, R.string.toast, Toast.LENGTH_LONG)
+                    .show();
+        });
+        check();
+
+        switchBangla.setOnCheckedChangeListener((compoundButton, b) -> {
+            if(b) {
+                setLocale("bn");
+            } else  {
+                setLocale("en");
+            }
+        });
+    }
+
+    public void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Resources resources = getResources();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = locale;
+        resources.updateConfiguration(configuration, displayMetrics);
+        Intent refresh = new Intent(this, MainActivity.class);
+        finish();
+        startActivity(refresh);
+
+
+    }
+
+    public void check() {
+        String locale = getResources().getConfiguration().locale.getLanguage();
+
+        if(locale == "bn") {
+            switchBangla.setChecked(true);
+        } else  {
+            switchBangla.setChecked(false);
+        }
     }
 }
