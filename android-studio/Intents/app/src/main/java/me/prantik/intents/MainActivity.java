872be0eba5bc7ac942a87cd6536a9992efd51f8c
Prantik <shahnoorprantik@gmail.com>
2021-07-24 07:03:10 +0600

    android-studio\SharedPreferences created

--- android-studio/Intents/app/src/main/java/me/prantik/intents/MainActivity.java
+++ android-studio/Intents/app/src/main/java/me/prantik/intents/MainActivity.java
@@ -2,13 +2,30 @@ package me.prantik.intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText name ;
+    Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.editTextText);
+        go = findViewById(R.id.button);
+
+        go.setOnClickListener(view -> {
+            String userName = name.getText().toString();
+
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putExtra("userName", userName);
+            startActivity(intent);
+        });
     }
 }
